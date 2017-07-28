1316faf37b8a6d406cac198a46e120b0dfeff7a5
Shweta Patrachari <shwetachari@gmail.com>
2017-07-28 13:17:53 -0700

    logs localStorage size every time info is stored/removed from storage

--- js/main.js
+++ js/main.js
@@ -24,10 +24,15 @@ if (localStorage["students"] !== "undefined") {
   ];
 }
 // Store
+// Each student takes up ~0.15KB local storage
+// Local Storage Limit = 4KB
+// Approx student capacity = 26 students
 function store() {
   if(studentArray !== undefined) {
     localStorage['students'] = JSON.stringify(studentArray);
   }
+  // each time information is added/removed to storage, display size of localStorage object and properties in console
+  var _lsTotal=0,_xLen,_x;for(_x in localStorage){_xLen= ((localStorage[_x].length + _x.length)* 2);_lsTotal+=_xLen; console.log(_x.substr(0,50)+" = "+ (_xLen/1024).toFixed(2)+" KB")};console.log("Total = " + (_lsTotal / 1024).toFixed(2) + " KB");
 }
 
 function renderStudentObject(firstName, lastName, stickers) {
